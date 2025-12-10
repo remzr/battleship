2ee745a6e05e7975c3a6afec1c2fda24490e6d0c
remzr <110097561+remzr@users.noreply.github.com>
2025-12-10 15:19:09 +0100

    Add class for ships

---
+++ src/ships.js
@@ -0,0 +1,17 @@
+export class ship {
+    sunk = false;
+    hits = 0;
+    constructor(length, owner) {
+        this.length = length;
+        this.owner = owner;
+    }
+    gotHit() {
+        this.hits ++
+        this.isSunk();
+    }
+    isSunk() {
+        if (this.hits == this.length) {
+            console.log(`${this.owner}s ${this.length}-sized ship has been Sunk`);
+        }
+    }
+}
